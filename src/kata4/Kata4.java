271d80d3974048069f76471513d0edaa73ccf085
usuario <usuario@10.22.143.47>
2016-10-28 09:49:02 +0100

    kata 4v3

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,24 +1,18 @@
 package kata4;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Kata4 {
 
-    public static void main(String[] args) {
-        
-        File file = new File("c:\\pub");
-        print(file.listFiles(),"");
-    }
-    
-    
-    private static void print(File[] files, String indent){
-        if(files == null) return;
-        for (File file : files) {
-            System.out.println(indent + (file.isDirectory() ? "+" : "-") + file.getName());
-            if(!file.isDirectory() || file.isHidden()) continue;
-            print(file.listFiles(), " ");
-            
-        }
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String filename = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\kata4\\emailsfilev1.txt";
+        ArrayList<String> mailList = MailListReader.read(filename);
+        Histogram<String> histogram = HistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
     }
     
 }
