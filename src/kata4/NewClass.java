5d2f76aa31fe71bea83062286e7a028ec4567428
usuario <usuario@10.22.143.47>
2016-10-28 08:55:02 +0100

    kata 1

---
+++ src/kata4/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata4;
+
+/**
+ *
+ * @author usuario
+ */
+public class NewClass {
+    
+}
